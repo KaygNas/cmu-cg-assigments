fdf78117b4488965571b5af3de6ac089ce34f08b
huangchengjie <597857074@qq.com>
2022-09-18 14:50:00 +0800

    DONE A1T1;

--- src/scene/transform.cpp
+++ src/scene/transform.cpp
@@ -13,17 +13,27 @@ Mat4 Transform::local_to_world() const {
 	//A1T1: local_to_world
 	//don't use Mat4::inverse() in your code.
 
-	return Mat4::I; //<-- wrong, but here so code will compile
+	if (std::shared_ptr<Transform> parent_ = parent.lock()) {
+		// rescursivle sum up all the transform of parents
+		return parent_->local_to_world() * local_to_parent();
+	} else {
+		return local_to_parent();
+	}
 }
 
 Mat4 Transform::world_to_local() const {
 	//A1T1: world_to_local
 	//don't use Mat4::inverse() in your code.
 
-	return Mat4::I; //<-- wrong, but here so code will compile
+	if (std::shared_ptr<Transform> parent_ = parent.lock()) {
+		// rescursivlesum up all the transform of parents
+		return  parent_to_local() * parent_->world_to_local();
+	} else {
+		return parent_to_local();
+	}
 }
 
 bool operator!=(const Transform& a, const Transform& b) {
 	return a.parent.lock() != b.parent.lock() || a.translation != b.translation ||
-	       a.rotation != b.rotation || a.scale != b.scale;
+		a.rotation != b.rotation || a.scale != b.scale;
 }
